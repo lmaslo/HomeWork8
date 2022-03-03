0df83347158e982742cc8e53f3a183570a2310d5
lmaslo <PerepechkinaEV@yandex.ru>
2022-03-03 12:56:50 +0300

    inti commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+
+    @Test
+    void assertTrueTest(){
+        assertTrue (3>2);
+    }
+}
